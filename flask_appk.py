67dcccba66b85daef8f7182422fbc4bb5bffe6cd
nitishsingh6277 <nitishsingh6277@gmail.com>
2023-06-18 18:47:20 +0530

    added the fix

--- flask_appk.py
+++ flask_appk.py
@@ -1,4 +1,4 @@
-from flask import  Flask
+from flask import  Flask, request
 
 app = Flask(__name__)
 
@@ -6,5 +6,15 @@ app = Flask(__name__)
 def hello_world():
     return "<h1>Hello world</h1>"
 
+@app.route("/h2")
+def hello_world2():
+    return "<h1> hello 2 world </h1>"
+
+@app.route("/test2")
+def test2():
+    data = request.args.get('x')
+    return 'this is the data u input in the url{}'.format(data)
+
 if __name__ == "__main__":
     app.run(host = "0.0.0.0")
+
